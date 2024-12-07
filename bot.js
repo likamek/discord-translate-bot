e89bf3ac7f35aa067a7d2d65b78fa2552dcce67f
Lika Mekinulov <Lika.Mekinulov@mail.huji.ac.il>
2024-12-07 22:51:44 +0200

    commit

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 import { Client, GatewayIntentBits, EmbedBuilder } from 'discord.js';
 import axios from 'axios';
 import { franc } from 'franc-min';
-import express from 'express';
 
 dotenv.config();
 
@@ -142,20 +141,6 @@ async function translateText(text, sourceLang, targetLang) {
 // Event handler when the bot is ready
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-
-    // Set up Express to keep the web service alive
-    const app = express();
-    const PORT = process.env.PORT || 3000;
-
-    // Simple route to keep the web service alive
-    app.get('/', (req, res) => {
-        res.send('Bot is running!');
-    });
-
-    // Listen on the specified port
-    app.listen(PORT, () => {
-        console.log(`Server is listening on port ${PORT}`);
-    });
 });
 
 // Event handler for message creation
